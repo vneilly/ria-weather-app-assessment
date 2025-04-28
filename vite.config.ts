94e6ccc90a3f9b53a5043d4f6a6ef2aeee79bd04
Vernon E. Neilly II <ventwo@gmail.com>
2025-04-27 22:51:47 -0700

    chore(project-setup): config path aliases, create env file, install setup dependencies for linting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from "path";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   plugins: [vue()],
-})
+});
