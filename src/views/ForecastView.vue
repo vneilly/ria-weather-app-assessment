e483fe7499025c02c479ad381a52a75519e32cf8
Vernon E. Neilly II <ventwo@gmail.com>
2025-04-30 10:10:04 -0700

    feat(tabs): scaffold ForecastView.vue for city tabs

---
+++ src/views/ForecastView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// Future imports and state will be added here
+</script>
+
+<template>
+  <!-- Tabs will be added here -->
+  <div class="forecast-view">
+    <h1>Forecast View</h1>
+    <p>This is the forecast view.</p>
+  </div>
+</template>
