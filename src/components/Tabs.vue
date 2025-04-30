6ff4d0c33753baa19a24de8195a5780076acab9e
Vernon E. Neilly II <ventwo@gmail.com>
2025-04-30 10:14:32 -0700

    feat(Tabs): scaffold the Tabs.vue component

---
+++ src/components/Tabs.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+// props and emits will be defined here
+import { defineProps, defineEmits } from "vue";
+</script>
+
+<template>
+  <!-- Tab buttons will go here -->
+</template>
